e115fca8b2104179753258875632e2137e51cc19
Roland Schäfer <roland.schaefer@fu-berlin.de>
2018-10-07 21:53:21 +0200

    Cosmetic fixes in dump sample.

--- samples/dump.py
+++ samples/dump.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 
+# This dumps a very raw concordance format.
+# It's very efficient, though.
+# See samply.py for options.
+
 from SeaCOW import Query, ConcordanceDumper
 
 q = Query()
 q.corpus          = 'decow16a-nano'
-q.string          = '[word="Hausfrau"]'
+q.string          = '[word="Chuzpe"]'
 q.max_hits        = 10
 q.attributes      = ['word']
 q.structures      = ['s']
-q.references      = ['doc.url', 'doc.bdc', 'doc.tld', 'doc.id', 'div.bpc', 's.idx', 's.type']
+q.references      = ['doc.url', 'doc.id', 's.idx']
 q.container       = 's'
 q.set_deduplication()
 
 p                 = ConcordanceDumper()
-p.filename        = 'hausfrau.csv'
+p.filename        = 'chuzpe.csv'
 q.processor       = p
 q.run()
-
